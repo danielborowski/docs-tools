4d64798642aa71003623e56c0473ee06beb160b5
Daniel Borowski <danielborowski@daniels-mbp-5.lan>
2020-01-02 12:18:48 -0500

    bind

--- themes/mongodb/src/js/componentEcosystemLinkTrack.js
+++ themes/mongodb/src/js/componentEcosystemLinkTrack.js
@@ -1,15 +1,19 @@
 import {reportAnalytics, toArray} from './util';
 
+function reportClick(anchorElement) {
+    reportAnalytics('Link Clicked', {
+        'text': anchorElement.text,
+        'href': anchorElement.href,
+    })
+}
+
 export function setup() {
     // only on ecosystem homepage, track link clicks
     if (document.body.dataset && document.body.dataset.project === 'ecosystem' && window.location.href === 'https://docs.mongodb.com/ecosystem/') {
         // get links on ecosystem
         const anchors = toArray(document.getElementsByTagName('a'));
         anchors.forEach(function(anchorElement) {
-            anchorElement.addEventListener('click', reportAnalytics('Link Clicked', {
-                'text': anchorElement.text,
-                'href': anchorElement.href,
-            }));
+            anchorElement.addEventListener('click', reportClick.bind(this, anchorElement));
         });
     }
 }
